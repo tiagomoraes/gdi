8b3c4c12ae931ccb7eb0e4f314b28dbed8414c35
Vinicius Dantas <vdj@cin.ufpe.br>
2019-10-08 16:41:43 -0300

    add trigger

---
+++ pl/19 e 20 - TRIGGER.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE TRIGGER check_formacao
+BEFORE INSERT ON farmaceutico
+FOR EACH ROW 
+
+BEGIN 
+IF :NEW.formacao_academica NOT IN ('Graduando', 'Graduado', 'Pos-Graduando', 'Pos-Graduado', 'Mestranda', 'Mestrando', 'Mestre', 'Doutor', 'Doutora', 'Doutorando', 'Doutoranda')
+    THEN RAISE_APPLICATION_ERROR(-20101, 'Formação acadêmica inválida');
+END IF;
+END;
+
+INSERT INTO farmaceutico (cpf_p, formacao_academica)
+            VALUES ('04304130170', 'Meme');
