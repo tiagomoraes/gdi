78ce6da85d3e3cb77e026d194649d899813d9a74
dxxglas <mcavalcante.douglas@gmail.com>
2019-10-08 12:14:18 -0300

    add 8 and 14

---
+++ pl/8 - IF ELSEIF.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION faixa_de_promocao
+RETURN VARCHAR2;
+IS
+    percent_promo NUMBER;
+    faixa_promo VARCHAR2(20);
+
+    CURSOR C IS
+        SELECT percentual
+        FROM promocao;
+BEGIN
+    OPEN C;
+    FETCH C INTO percent_promo;
+    CLOSE C;
+
+    IF percent_promo <= 10 THEN
+        faixa_promo := 'Desconto';
+    ELSIF percent_promo > 10 and percent_promo <= 50 THEN
+         faixa_promo := 'Promoção Incrível';    
+    ELSIF percent_promo > 50 and percent_promo < 100 THEN
+        faixa_promo := 'Saldão';
+    ELSE
+        faixa_promo := 'Grátis';
+    END IF;
+
+    RETURN faixa_promo;
+END;
