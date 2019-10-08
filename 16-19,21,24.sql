fad1b3c9084d34557f2264a77308c2a4caf54e17
Vinicius Dantas <vdj@cin.ufpe.br>
2019-10-07 21:26:57 -0300

    ajustando nome do arquivo

--- 16-19,21,24.sql
+++ 16-19,21,24.sql
@@ -12,7 +12,7 @@ WHERE f.salario < (SELECT AVG (salario) FROM funcionario)
 --Compostos cujo o custo é maior que o todos os preços dos produtos
 SELECT c.nome, c.custo
 FROM composto c
-WHERE c.custo > ALL (SELECT peso FROM produto)
+WHERE c.custo > ALL (SELECT preco FROM produto)
 
 --peso e o nome do remedio com menor peso
 SELECT p.peso, p.nome
