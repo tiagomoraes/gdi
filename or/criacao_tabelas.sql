35239f69205b7d43a542c1424ce5de0fd2f412cc
Vinicius Dantas <vdj@cin.ufpe.br>
2019-11-11 20:25:31 -0300

    falta resolver a nested table

--- or/criacao_tabelas.sql
+++ or/criacao_tabelas.sql
@@ -1,33 +1,43 @@
 CREATE TABLE tb_farmaceutico OF tp_farmaceutico (
     cpf PRIMARY KEY
 );
+/
 
 CREATE TABLE tb_balconista OF tp_balconista (
     cpf PRIMARY KEY
 );
+/
 
 CREATE TABLE tb_cliente OF tp_cliente (
     cpf PRIMARY KEY
 );
+/
 
-CREATE TABLE tb_fornecedor OF tp_fornecedor (
-    cnpj PRIMARY KEY
-);
+--CREATE TABLE tb_fornecedor OF tp_fornecedor NESTED TABLE telefones STORE AS nt_telefones;
+--/
+
+--CREATE TABLE tb_fornecedor OF tp_fornecedor (
+--    cnpj PRIMARY KEY
+--);
+--/
 
 CREATE TABLE tb_composto OF tp_composto (
     id PRIMARY KEY
 );
+/
 
 CREATE TABLE tb_medicamento_manipulado OF tp_medicamento_manipulado (
     id PRIMARY KEY
 );
+/
 
 CREATE TABLE tb_produto OF tp_produto (
     id PRIMARY KEY
 );
+/
 
 CREATE TABLE tb_promocao of tp_promocao (
     id PRIMARY KEY  
-)
+);
+/
 
-CREATE TABLE tb_fornecedor OF tp_fornecedor NESTED TABLE telefones STORE AS nt_telefones;
