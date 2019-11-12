e430cfc11057ee56f3c15a4157fd53438dfaf83b
Vinicius Dantas <vdj@cin.ufpe.br>
2019-11-12 16:21:05 -0300

    farmaceuticos adicionados

--- or/povoamento.sql
+++ or/povoamento.sql
@@ -1,3 +1,5 @@
+--INSERINDO FARMACEUTICO
+
 INSERT INTO tb_farmaceutico VALUES(
     tp_farmaceutico (
         '04304130170', 
@@ -25,7 +27,7 @@ INSERT INTO tb_farmaceutico VALUES(
     tp_farmaceutico (
         '94512104909', 
         'Shikamaru', 
-        to_date('23/07/00', 'dd/mm/yy'), 
+        to_date('22/09/00', 'dd/mm/yy'), 
         varray_fone(tp_fone('767883214')),
         '04304130170', 
         5342, 
@@ -34,12 +36,81 @@ INSERT INTO tb_farmaceutico VALUES(
         16, 
         tp_endereco(
             '55153065',
-            '29',
-            'Apt 201',
+            '38',
+            'Apt 402',
             'Bom Conselho',
             'Belo Jardim'
         ),
         'Doutorando'
+    )
+);
+/
+
+INSERT INTO tb_farmaceutico VALUES(
+    tp_farmaceutico (
+        '19876905910', 
+        'Gaara', 
+        to_date('19/01/00', 'dd/mm/yy'), 
+        varray_fone(tp_fone('365154575')),
+        '94512104909', 
+        7456, 
+        to_date('19/08/02', 'dd/mm/yy'), 
+        40, 
+        17, 
+        tp_endereco(
+            '55010280',
+            '80',
+            'Apt 209',
+            'Divinopolis',
+            'Caruaru'
+        ),
+        'Mestre'
+    )
+);
+/
+
+INSERT INTO tb_farmaceutico VALUES(
+    tp_farmaceutico (
+        '26467070615', 
+        'Hinata', 
+        to_date('27/12/00', 'dd/mm/yy'), 
+        varray_fone(tp_fone('335850308')),
+        '19876905910', 
+        4578, 
+        to_date('20/08/02', 'dd/mm/yy'), 
+        48, 
+        18, 
+        tp_endereco(
+            '55644510',
+            '23',
+            'Apt 102',
+            'Cruzeiro',
+            'Gravatá'
+        ),
+        'Mestranda'
+    )
+);
+/
+
+INSERT INTO tb_farmaceutico VALUES(
+    tp_farmaceutico (
+        '58142191571', 
+        'Neji', 
+        to_date('03/07/00', 'dd/mm/yy'), 
+        varray_fone(tp_fone('934619008')),
+        '26467070615', 
+        999, 
+        to_date('26/08/02', 'dd/mm/yy'), 
+        40, 
+        11, 
+        tp_endereco(
+            '56512360',
+            '73',
+            'Apt 502',
+            'Sao Cristovao',
+            'Arcoverde'
+        ),
+        'Pos-graduado'
     )
 );
 /
