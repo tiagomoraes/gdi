6069870d677de77e12aeb0bfe32fdd4257731eee
Eidson Jacques Almeida de Sa <ejas@WINDOWS.CIN.UFPE.BR>
2019-11-28 12:22:19 -0300

    novos itens do checklist resolvidos

--- mongo/loadPackagesDataset.js
+++ mongo/loadPackagesDataset.js
@@ -1,5 +1,4 @@
-// 1 2 3 4 5 6 7 8 10 14 16 19
-
+// 1 
 use packages;
 
 db.packages.insertMany([
@@ -7,22 +6,39 @@ db.packages.insertMany([
   { country: "Egypt", price: 5430, cities: ["Cairo", "Alexandria", "Luxor"], daysStaying: 10 },
   { country: "China", price: 5021, cities: ["Beijing", "Shanghai"], daysStaying: 10 },
   { country: "Canada", price: 6031, cities: ["Toronto", "Montreal", "Vancouver"], daysStaying: 10 },
-  { country: "Canada", price: 6532, cities: ["Calgary", "Quebec"], daysStaying: 10 }
+  { country: "Canada", price: 6532, cities: ["Calgary", "Quebec"], daysStaying: 10, discount: "Y" }
 ]);
 
+// 26
+db.packages.save( { country: "Pernambuco", price: 1234, cities: ["Recife", "Arcoverde", "Caruaru"], daysStaying: 10  } ) // meu país pernambuco
+
+// 21 25
+db.packages.update(
+  { country: "Peru"},
+  {
+    $set: {
+      daysStaying: 12
+    }
+  }
+)
+
+// 2 14 19
 db.packages.find().pretty().sort({ price: -1 });
 
+// 10
 db.packages.find().count();
 
+// 3
 db.packages.find({ cities: { $size: 2 } });
 
+// 29 16
 db.packages.findOne({
   $where: function () {
     return (this.country == 'Peru');
   }
 });
 
-
+// 4 5 7 8 9
 db.packages.aggregate([
   { $match: { price: { $gte: 6000 } } },
   {
@@ -33,6 +49,32 @@ db.packages.aggregate([
   }
 ]);
 
+// 6
 db.packages.aggregate([
   { $project: { _id: 0, country: 1, price: 1, daysStaying: 1 } }
 ]);
+
+// 13
+db.packages.find( { discount: { $exists: true} } );
+
+// 22 23
+db.packages.find( { $text: { $search: "Cairo"} } );
+
+// 20
+db.packages.find( { cities: { $all: ["Calgary", "Quebec"] } } );
+
+//30
+db.packages.update(
+  {country: "China"},
+  { $addToSet: { cities: "Wuxi"} }
+);
+
+
+
+
+
+
+
+
+// 27
+db.packages.renameCollection("pacotes");
