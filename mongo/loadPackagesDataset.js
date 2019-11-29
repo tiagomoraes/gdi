006f551e8bca8afc98e6f60e83e4e8f01c0fb525
Tiago Moraes <tiago3902@gmail.com>
2019-11-28 23:46:19 -0300

    Added hotels to populate

--- mongo/loadPackagesDataset.js
+++ mongo/loadPackagesDataset.js
@@ -2,25 +2,25 @@
 use packages;
 
 db.packages.insertMany([
-  { country: "Peru", price: 3210, cities: ["Lima", "Cusco", "Machu Picchu"], daysStaying: 10,  },
-  { country: "Egypt", price: 5430, cities: ["Cairo", "Alexandria", "Luxor"], daysStaying: 10 },
-  { country: "China", price: 5021, cities: ["Beijing", "Shanghai"], daysStaying: 10 },
+  { country: "Peru", price: 3210, cities: ["Lima", "Cusco", "Machu Picchu"], daysStaying: 10,  hotels: [{name: "Cusco Hotel", daysStaying: 3}, {name: "BananaLife Resort", daysStaying: 4}, {name: "Hanah Montanna Hostel", daysStaying: 1}]},
+  { country: "Egypt", price: 5430, cities: ["Cairo", "Alexandria", "Luxor"], daysStaying: 10, hotels: [{name: "Cairo Suites", daysStaying: 2}, {name: "Holiday Inn", daysStaying: 1}]},
+  { country: "China", price: 5021, cities: ["Beijing", "Shanghai"], daysStaying: 10, hotels: [{name: "ChinaCity", daysStaying: 2}, {name: "Tai Chin Suan", daysStaying: 5}, {name: "Chau Pin", daysStaying: 1}] },
   { country: "Canada", price: 6031, cities: ["Toronto", "Montreal", "Vancouver"], daysStaying: 10 },
   { country: "Canada", price: 6532, cities: ["Calgary", "Quebec"], daysStaying: 10, discount: "Y" },
   { country: "Canada", price: 7420, cities: ["Quebec", "Toronto", "Vancouver"], daysStaying: 14},
-  { country: "Canada", price: 6956, cities: ["Ottawa",  "Montreal", "Quebec"], daysStaying: 9},
+  { country: "Canada", price: 6956, cities: ["Ottawa",  "Montreal", "Quebec"], daysStaying: 9, hotels: [{name: "Montepelier Suites", daysStaying: 2}, {name: "Home Inn Suites", daysStaying: 4}]},
   { country: "Russia", price: 7200, cities: ["Moscow", "Saint Petersburg", "Kazan", "Kaliningrad", "Vladivostok"], daysStaying: 14, discount: "Y" },
   { country: "United Kingdom", price: 6200, cities: ["London", "Manchester", "Glasgow", "Liverpool"], daysStaying: 7},
   { country: "United Kingdom", price: 5550, cities: ["London", "Birmingham", "Sheffield"], daysStaying: 5, discount: "Y" },
-  { country: "France", price: 8321, cities: ["Paris", "Marseille", "Lyon", "Nice"], daysStaying: 10 },
+  { country: "France", price: 8321, cities: ["Paris", "Marseille", "Lyon", "Nice"], daysStaying: 10, hotels: [{name: "Le Monde Hostel", daysStaying: 10}] },
   { country: "France", price: 6794, cities: ["Marseille", "Lyon", "Nice", "Nantes", "Toulon"], daysStaying: 7, discount: "Y" },
   { country: "Portuagal", price: 6400, cities: ["Lisboa", "Porto", "Coimbra", "Évora"], daysStaying: 6, discount: "Y" },
-  { country: "Denmark", price: 7469, cities: ["Copenhagen", "Odense", "Kolding"], daysStaying: 8},
+  { country: "Denmark", price: 7469, cities: ["Copenhagen", "Odense", "Kolding"], daysStaying: 8, hotels: [{name: "Cold Box Suites", daysStaying: 1}, {name: "Freezing Death", daysStaying: 3}]},
   { country: "Bolivia", price: 3839, cities: ["Santa Cruz", "La Paz", "Potosí"], daysStaying: 7, discount: "Y" },
-  { country: "Argentina", price: 2932, cities: ["Rosario", "Córdoba", "La Plata", "Tucumán"], daysStaying: 6, discount: "Y" },
-  { country: "Mexico", price: 4121, cities: ["Mexico City", "Guadalajara", "Juárez"], daysStaying: 5, discount: "Y" },
-  { country: "Mexico", price: 6532, cities: ["Mexico City", "Guadalajara", "Cancún"], daysStaying: 14},
-  { country: "United States", price: 7132, cities: ["Los Angeles", "San Diego", "San Jose", "San Francisco"], daysStaying: 10},
+  { country: "Argentina", price: 2932, cities: ["Rosario", "Córdoba", "La Plata", "Tucumán"], daysStaying: 6, discount: "Y", hotels: [{name: "Dom Pablito", daysStaying: 4}, {name: "Dom Juan Hotel", daysStaying: 1}, {name: "Malaga Rooms", daysStaying: 1}] },
+  { country: "Mexico", price: 4121, cities: ["Mexico City", "Guadalajara", "Juárez"], daysStaying: 5, discount: "Y", hotels: [{name: "Fuego de la Playa", daysStaying: 5}] },
+  { country: "Mexico", price: 6532, cities: ["Mexico City", "Guadalajara", "Cancún"], daysStaying: 14, hotels: [{name: "Fuego de la Playa", daysStaying: 15}]},
+  { country: "United States", price: 7132, cities: ["Los Angeles", "San Diego", "San Jose", "San Francisco"], daysStaying: 10, hotels: [{name: "Holliday Inn", daysStaying: 3}, {name: "Bon Voyage House & Hotels", daysStaying: 2}]},
   { country: "United States", price: 8123, cities: ["Las Vegas", "New York", "Chicago"], daysStaying: 13},
   { country: "Ejasland", price: 999999, cities: ["Disney", "Rip rbu", "Monster Energy", "Edmilson Town"], daysStaying: 666}
 ]);
@@ -129,7 +129,7 @@ db.packages.aggregate([
         $filter: {
           input: "$hotels",
           as: "hotel",
-          cond: { $gte: ["$$hotel.daysStaying", 2] }
+          cond: { $gte: ["$$hotel.daysStaying", 3] }
         }
       }
     }
